e4770beb7d1906cdf28c5489ce24194a75be5fee
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-09-13 00:13:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 
+var playing=false;
+var score;
+var life;
+$(document).ready(function(){
+
+  $("#start").click(function(){
+      if(playing==true)
+         {
+             location.reload();
+         } else
+         {
+             playing = true;
+             score=0;
+             $("#scorevalue").html(score);
+             life();
+             $("#start").html("Reset Game");
+
+         }
+  });
+});
+
+function life()
+{
+    for(i=0;i<3;i++)
+    {
+        $("#life").append(" x ");
+    }
+}
